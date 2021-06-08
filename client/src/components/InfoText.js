88e403e0740666a4b5d0655389781e1206eecc41
ry819 <ry819@ic.ac.uk>
2021-06-08 18:20:49 +0800

    Changed wording of InfoText

--- client/src/components/InfoText.js
+++ client/src/components/InfoText.js
@@ -1,8 +1,16 @@
 import React from "react";
 
 export default function InfoText(props) {
+<<<<<<< Updated upstream:client/src/components/InfoText.js
   return (<p> The store name is {props.storeName}.
     The location is {props.location}.
     The language spoken is {props.language}. </p>
   );
 }
+=======
+    return (<p> Store name {props.storeName}. 
+    Location :{props.location}. 
+    Languages: {props.language}. </p>
+    );
+  }
+>>>>>>> Stashed changes:client/src/InfoText.js
