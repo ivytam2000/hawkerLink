f18d3faa314c5aedfdd01b97cb5da8274adec571
ry819 <ry819@ic.ac.uk>
2021-06-09 20:52:39 +0800

    fixed image issues

--- client/src/pages/AssistMain/AssistMain.js
+++ client/src/pages/AssistMain/AssistMain.js
@@ -4,6 +4,11 @@ import {SearchHawker} from '../../services/AssistHawker';
 import {HawkerSearchResults} from '../../components/HawkerSearchResults';
 import './AssistMain.css';
 import {Link} from "react-router-dom";
+import register from './assist-icons/register.png'
+import meet from './assist-icons/meet.png'
+import choose from './assist-icons/choose.png'
+import signup from './assist-icons/signup.png'
+import hours from './assist-icons/hours.png'
 
 export function AssistMainPage() {
     return (
@@ -16,23 +21,23 @@ export function AssistMainPage() {
         <table className="icons">
             <tr>
                 <td class="icon">
-                    <img src="./assist-icons/register.png" alt= "Register"width="100px"></img>
+                    <img src={register} alt= "Register"width="100px"></img>
                     <p className="caption">1. Find hawkers suitable for you and sign up to help them</p>
                 </td> 
                 <td class="icon">
-                    <img src="./assist-icons/meet.png" alt="Meet" width="100px"></img>
+                    <img src={meet} alt="Meet" width="100px"></img>
                     <p className="caption">2. Arrange a meeting with your allocated hawker</p>
                 </td>
                 <td class="icon">
-                    <img src="./assist-icons/choose.png" alt="Choose" width="100px"></img>
+                    <img src={choose} alt="Choose" width="100px"></img>
                     <p className="caption">3. Help them to choose a food delivery service</p>
                 </td>
                 <td class="icon">
-                    <img src="./assist-icons/signup.png" alt="Signup" width="100px"></img>
+                    <img src={signup} alt="Signup" width="100px"></img>
                     <p className="caption">4. Register their business with the chosen food delivery service</p>
                 </td>
                 <td class="icon">
-                    <img src="./assist-icons/hours.png" alt="Hours" width="100px"></img>
+                    <img src={hours} alt="Hours" width="100px"></img>
                     <p className="caption">5. Claim your CIP hours (if applicable)</p>
                 </td>
             </tr>
