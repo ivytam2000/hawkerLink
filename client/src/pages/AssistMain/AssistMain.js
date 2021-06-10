e6ba7a7095f49519d2ee9f5f32d7c487fa35dbb5
Zhai Zirun <zz4319@ic.ac.uk>
2021-06-10 16:19:41 +0800

    [zz4319] modified icons in assistmain

--- client/src/pages/AssistMain/AssistMain.js
+++ client/src/pages/AssistMain/AssistMain.js
@@ -4,55 +4,83 @@ import {SearchHawker} from '../../services/AssistHawker';
 import {HawkerSearchResults} from '../../components/HawkerSearchResults';
 import './AssistMain.css';
 import {Link} from "react-router-dom";
+import introvid from './elevator_pitch.mov'
 import register from './assist-icons/register.png'
 import meet from './assist-icons/meet.png'
 import choose from './assist-icons/choose.png'
-import signup from './assist-icons/signup.png'
-import hours from './assist-icons/hours.png'
+import signup from './assist-icons/deliveroo.jpeg'
+import hours from './assist-icons/claim.png'
+import arrow from './assist-icons/right-arrow.png'
 
 export function AssistMainPage() {
     return (
         <body>
         <GenericLayout />
-        <div className="title">
+        {/* <div className="title">
             <p>There are many invisible hawkers in Singapore who are unable to get on food delivery sites and have seen their business plummet. We 
                 here at HawkerLink hope to connect young volunteers with these hawkers to help guide them through the signup process. 
             </p>
+        </div> */}
+        <div className="vid">
+            <video controls width="100%">
+                <source src={introvid} type="video/mov"></source>
+            </video>
         </div>
+
         <div className="title">
-            <p>Here's how you can get involved ♥</p>
+            <p>How to get involved:</p>
         </div>
 
         <table className="icons">
             <tr>
                 <td class="icon">
-                    <img src={register} alt= "Register"width="100px"></img>
-                    <p className="caption">1. Find hawkers suitable for you and sign up to help them</p>
+                    <img src={register} alt= "Register" width="100px" height="100px"></img>
+                    <Link to="/findhawker">
+                    <p><button className="step">Sign up</button></p>
+                    </Link>
                 </td> 
+
+                <td class="arrow">
+                <img src={arrow} alt= "Arrow" width="30px"></img>
+                </td>
+
                 <td class="icon">
                     <img src={meet} alt="Meet" width="100px"></img>
-                    <p className="caption">2. Arrange a meeting with your allocated hawker</p>
+                    <p><button className="step">Meet up</button></p>
                 </td>
+
+                <td class="arrow">
+                <img src={arrow} alt= "Arrow" width="30px"></img>
+                </td>
+
                 <td class="icon">
                     <img src={choose} alt="Choose" width="100px"></img>
-                    <p className="caption">3. Help them to choose a food delivery service</p>
+                    <p><button className="step">Help them choose a service</button></p>
                 </td>
+
+                <td class="arrow">
+                <img src={arrow} alt= "Arrow" width="30px"></img>
+                </td>
+
                 <td class="icon">
                     <img src={signup} alt="Signup" width="100px"></img>
-                    <p className="caption">4. Register their business with the chosen food delivery service</p>
+                    <p><button className="step">Register with the service</button></p>
                 </td>
+
+                <td class="arrow">
+                <img src={arrow} alt= "Arrow" width="30px"></img>
+                </td>
+
                 <td class="icon">
                     <img src={hours} alt="Hours" width="100px"></img>
-                    <p className="caption">5. Claim your CIP hours (if applicable)</p>
+                    <p><button className="step">Claim CIP hours</button></p>
                 </td>
             </tr>
         </table>
 
         <div className="exp">
-            <p>Click on the steps above to skip to it!</p>
-            <p>Alternatively,</p>
             <Link to="/findhawker">
-            <button>Start by finding a hawker</button> 
+            <button className="start-button">Start by finding a hawker</button> 
             </Link>
         </div>
         
