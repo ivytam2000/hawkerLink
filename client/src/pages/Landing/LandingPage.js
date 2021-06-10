da64ed7bb2db5d31b319acb35973790e90ed9c9d
Zhai Zirun <zz4319@ic.ac.uk>
2021-06-11 04:30:06 +0800

    [zz4319] merged assistmain and search bar, and changed landing page

---
+++ client/src/pages/Landing/LandingPage.js
@@ -0,0 +1,22 @@
+import {GenericLayout} from '../Layout';
+import HawkerSearchBar from '../../components/HawkerSearchBar';
+import {SearchHawker} from '../../services/AssistHawker';
+import {HawkerSearchResults} from '../../components/HawkerSearchResults';
+import './LandingPage.css';
+import {Link} from "react-router-dom";
+
+export function LandingPage() {
+    return (
+        <body>
+        <GenericLayout />
+        <div className="graphic">
+            <div className="graphic-wrapper">
+            <div className="graphic-text">
+                <b>hawkerlink</b><br></br>
+                connects volunteers with elderly hawkers to help them register with food delivery platforms.
+            </div>
+            </div>
+        </div>
+        </body>
+    );
+}
