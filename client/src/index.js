a8e213ef9c0f95739add30f9632975d751597a93
Zhai Zirun <zz4319@ic.ac.uk>
2021-06-16 17:04:30 +0800

    [zz4319] changed bg of landing and reshuffled position of user journey graphic

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { LandingPage } from './pages/Landing/LandingPage';
 import { AssistMainPage } from './pages/AssistMain/AssistMain';
+import { SearchHawkerPage } from './pages/SearchHawker/SearchHawker';
 import { SignUpPage } from './pages/Forms/SignUp';
 import { SuggestionPage } from './pages/Forms/Suggestion';
 import { ResourcesPage } from './pages/Resources/Resources'
@@ -18,6 +19,7 @@ import { ResourcesVerificationPage } from './pages/Resources/ResourcesVerified';
       <Switch>
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/assist" component={AssistMainPage} />
+        <Route exact path="/search" component={SearchHawkerPage} />
         <Route exact path="/suggesthawker" component={SuggestionPage} />
         <Route path="/signup" component={SignUpPage} />
         <Route path="/resources" component={ResourcesPage} />
