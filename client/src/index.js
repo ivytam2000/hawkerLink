b27ca5f3f6a63182feffa34f2119a697dda2a84e
Zhai Zirun <zz4319@ic.ac.uk>
2021-06-22 02:28:49 +0800

    [zz4319] deleted irrelevant files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
@@ -33,14 +32,4 @@ import { BookingPage } from './pages/BookingPage';
    rootElement
  )
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
