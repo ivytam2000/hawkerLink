ac14ceb3055af0b9e9c655288f441da5857c6d98
ry819 <ry819@ic.ac.uk>
2021-06-23 22:09:40 +0800

    CHanged cip form

--- client/src/pages/Forms/Cip.js
+++ client/src/pages/Forms/Cip.js
@@ -1,4 +1,5 @@
 import {GenericLayout} from '../Sitewide/Layout';
+import { Link, useHistory } from "react-router-dom";
 import './Form.css';
 import React from "react";
 
@@ -25,15 +26,12 @@ export function Cip(props) {
             <p><span class="label" for="language">Hours Spent Helping Them</span>
             <input type="form-text" id="language" name="language"></input></p>
 
-            <p><span class="label" for="other">
-            Simply submit the required information with a photo containing the number of hours you have spent and a signature by the hawker. We will then contact your schools directly to help you credit the hours. An email will be sent to you to inform you when the verification is complete.
-            </span></p>
-            <p><span class="label" for="other">Screenshot of hours and hawker endorsement</span></p>
-            <input type="upload" value="Upload"></input>
             <p><span class="label" for="other">Email Address</span></p>
             <p><input type="form-text" id="language" name="language"></input></p>
          
-            <input type="submit" value="Submit"></input>
+          <Link to="/resources"><button className="search-btn">
+            Submit
+            </button>  </Link>
             </form> 
             <div>
   </div>
